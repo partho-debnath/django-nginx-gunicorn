c8783cc96a9c549ca2098b6d6624d4bf87c54519
Partho Debnath <parthodebnath28@gmail.com>
2024-03-24 16:27:22 +0600

    added django restframework for serving media files

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import home
+from .views import home, ImageListAPIView
 
 urlpatterns = [
     path(
         "",
         home,
         name="home",
+    ),
+    path(
+        "images/",
+        ImageListAPIView.as_view(),
+        name="images",
     ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
 from app.models import Image
+from .serializer import ImageSerializer
 
 
 def home(request):
     context = {"images": Image.objects.all()}
     return render(request, "index.html", context)
+
+
+class ImageListAPIView(ListAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
